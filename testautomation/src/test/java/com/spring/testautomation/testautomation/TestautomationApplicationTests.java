4147946ca29a447dcac97b233b9afd6d8dffa3c1
ansuman <ansuman.automation@gmail.com>
2021-11-16 19:56:23 +0530

    spring boot project created

---
+++ testautomation/src/test/java/com/spring/testautomation/testautomation/TestautomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.testautomation.testautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestautomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
